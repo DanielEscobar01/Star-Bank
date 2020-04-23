a8531e6d394b9a6adaea2e3d73a3eba576f7472e
danielescobar <danielescobar@localhost>
2020-04-22 21:19:46 -0500

    App

--- src/Controllers/Cashier.java
+++ src/Controllers/Cashier.java
@@ -5,23 +5,9 @@
  */
 package Controllers;
 
-import Accounts.*;
-import Customers.*;
+
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 
 /**
  * This class let us create a cashier
@@ -34,100 +20,6 @@ public class Cashier {
     String json;
 
     /**
-     * This method let us get the JSON of savings accounts
-     *
-     * @return The string with savings accounts JSON
-     */
-    public String returnJsonSavings() {
-        String json = "";
-        try (BufferedReader br = new BufferedReader(new FileReader("savingsAccounts.json"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                json += line;
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        if (json.length() == 0) {
-            return "[]";
-        } else {
-            return json;
-        }
-    }
-
-    /**
-     * This method let us get the JSON of checking accounts
-     *
-     * @return The string with checking accounts JSON
-     */
-    public String returnJsonCheckings() {
-        String json = "";
-        try (BufferedReader br = new BufferedReader(new FileReader("checkingsAccounts.json"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                json += line;
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        if (json.length() == 0) {
-            return "[]";
-        } else {
-            return json;
-        }
-    }
-
-    /**
-     * This method let us write a new account into savings account JSON
-     *
-     * @param json The string of JSON file
-     * @param account The string with values of account
-     */
-    public static void writeSavings(String json, String account) {
-        String auxiliar = json.substring(0, json.length() - 1); //Removes the last ]
-        if (json.length() == 2) {
-            auxiliar = auxiliar + account;
-        } else {
-            auxiliar = auxiliar + "," + account;
-        }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("savingsAccounts.json"))) {
-            bw.write(auxiliar);
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    /**
-     * This method let us write a new account into checking account JSON
-     *
-     * @param json The string of JSON file
-     * @param account The string with values of account
-     */
-    public static void writeCheckings(String json, String account) {
-        String auxiliar = json.substring(0, json.length() - 1); //Removes the last ]
-        if (json.length() == 2) {
-            auxiliar = auxiliar + account;
-        } else {
-            auxiliar = auxiliar + "," + account;
-        }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("checkingsAccounts.json"))) {
-            bw.write(auxiliar);
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    /**
      * This method let us add an account to JSON
      *
      * @param id The identification of the customer account
@@ -136,10 +28,16 @@ public class Cashier {
      */
     public void addAccount(String id, String titularId, String typeAccount) {
         Verify verification = new Verify();
-        verification.addAccount(id, titularId, typeAccount);  
+        verification.addAccount(id, titularId, typeAccount);
     }
 
-    public void deactivateAccount() {
+    /**
+     * This method let us deactivate an account
+     * @param id The identification of the account to be deleted
+     * @param titularId The identification of the titular of the account
+     * @param typeAccount The type of account to be deleted
+     */
+    public void deactivateAccount(String id, String titularId, String typeAccount) {
 
     }
 
--- src/Controllers/Verify.java
+++ src/Controllers/Verify.java
@@ -13,15 +13,96 @@ import com.google.gson.Gson;
 
 /**
  * This class will let us check if the parameters are okay to be saved
+ *
  * @author danielescobar
  */
 public class Verify {
-    
-    
-    
-    
-    
-    
+
+    Gson gson = new Gson();
+    String json;
+    Database database = new Database();
+
+    /**
+     * This method let us know if the account exist
+     *
+     * @param id The identification of the account
+     * @param typeAccount The type of account
+     * @return True if the account exist, False if not
+     */
+    public boolean existAccountId(String id, String typeAccount) {
+        switch (typeAccount) {
+            case "Savings Account":
+                SavingsAccount[] savingsAccounts = gson.fromJson(json, SavingsAccount[].class);
+                for (int i = 0; i < savingsAccounts.length; i++) {
+                    if (savingsAccounts[i].getId().equals(id)) {
+                        return true;
+                    }
+                }
+                break;
+            case "Checkings Account":
+                CheckingAccount[] checkingsAccounts = gson.fromJson(json, CheckingAccount[].class);
+                for (int i = 0; i < checkingsAccounts.length; i++) {
+                    if (checkingsAccounts[i].getId().equals(id)) {
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    /**
+     * This method let us check if the titular of the account is correct
+     * @param titularId     The identification of the titular of the account
+     * @param id    The identification of the account
+     * @param typeAccount   The type of account to be checked   
+     * @return True if the titular identification of the account is the same as the parameter, False if not.
+     */
+    public boolean isCorrectTitularId(String titularId, String id, String typeAccount) {
+        switch (typeAccount) {
+            case "Savings Account":
+                SavingsAccount[] savingsAccounts = gson.fromJson(json, SavingsAccount[].class);
+                for (int i = 0; i < savingsAccounts.length; i++) {
+                    if (savingsAccounts[i].getId().equals(id)) {
+                        if (savingsAccounts[i].getTitularId().equals(titularId)) {
+                            return true;
+                        }
+                    }
+                }
+                break;
+            case "Checkings Account":
+                CheckingAccount[] checkingsAccounts = gson.fromJson(json, CheckingAccount[].class);
+                for (int i = 0; i < checkingsAccounts.length; i++) {
+                    if (checkingsAccounts[i].getId().equals(id)) {
+                        if (checkingsAccounts[i].getTitularId().equals(titularId)) {
+                            return true;
+                        }
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    public String getSavings() {
+        return database.returnJsonSavings();
+    }
+
+    public String getCheckings() {
+        return database.returnJsonCheckings();
+    }
+
+    /**
+     * This method let us deactivate an account
+     *
+     * @param id The identification of the account to be deleted
+     * @param titularId The identification of the titular of the account
+     * @param typeAccount The type of account to be deleted
+     */
+    public void deactivateAccount(String id, String titularId, String typeAccount) {
+
+    }
+
     /**
      * This method let us add an account to JSON
      *
@@ -30,7 +111,6 @@ public class Verify {
      * @param typeAccount The type of account
      */
     public void addAccount(String id, String titularId, String typeAccount) {
-        Database database = new Database();
         Gson gson = new Gson();     //Object to implements GSON API
         // Filtering information by type of account with cases
         switch (typeAccount) {
@@ -48,5 +128,5 @@ public class Verify {
                 break;
         }
     }
-    
+
 }
